9cd8329b4e7ed6cff18ffc1c598fcb034fc201a2
joseph-gansukh <g.sukhbat@gmail.com>
2019-10-21 15:56:32 -0500

    added map styling

--- js/index.js
+++ js/index.js
@@ -8,25 +8,25 @@
 //   navigator.geolocation.getCurrentPosition(geoSuccess);
 // };
 
-window.onload = function() {
-  var startPos;
-  var geoOptions = {
-    enableHighAccuracy: true
-  }
+// window.onload = function() {
+//   var startPos;
+//   var geoOptions = {
+//     enableHighAccuracy: true
+//   }
 
-  var geoSuccess = function(position) {
-    startPos = position;
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  };
-  var geoError = function(error) {
-    console.log('Error occurred. Error code: ' + error.code);
-    // error.code can be:
-    //   0: unknown error
-    //   1: permission denied
-    //   2: position unavailable (error response from location provider)
-    //   3: timed out
-  };
+//   var geoSuccess = function(position) {
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   var geoError = function(error) {
+//     console.log('Error occurred. Error code: ' + error.code);
+//     // error.code can be:
+//     //   0: unknown error
+//     //   1: permission denied
+//     //   2: position unavailable (error response from location provider)
+//     //   3: timed out
+//   };
 
-  navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-};
+//   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+// };
